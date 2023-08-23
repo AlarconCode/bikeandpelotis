f7f708f1671fbe47c761f54e7805ef1abbc8dfeb
AlarconCode <julian.alarcon81@gmail.com>
2023-08-23 10:34:25 +0200

    install tailwind

--- bikeandpelotis/src/App.jsx
+++ bikeandpelotis/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Inicio from './Pages/Inicio'
 import Inscribete from './Pages/Inscribete'
+
 import './App.css'
 
 
--- bikeandpelotis/src/Header.jsx
+++ bikeandpelotis/src/Header.jsx
@@ -1,13 +1,15 @@
-
+import logo_ByP from './assets/logo-bikeandpelotis.svg'
+import menuIcon from './assets/Icono-menu-hamburguer-blanco.svg'
 
 function Header() {
     return (
-        <header>
-            <img 
-            className='logo-byp'
-            alt='logo club ciclista bike and pelotis'
-            src={logo_ByP} 
-            />
+        <header className='bg-black border-b border-slate-600 p-4 w-screen flex justify-between shadow-slate-300'>
+        
+            <img className='w-1/2'src={logo_ByP} />
+            <img className='w-10 hover:rotate-180 cursor-pointer mr-4' src={menuIcon} />
+    
         </header>
     )
 }
+
+export default Header

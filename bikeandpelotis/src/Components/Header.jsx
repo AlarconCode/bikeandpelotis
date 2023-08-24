c9be935184c5ab44bc88a5eed65f6e8542c9ab25
AlarconCode <julian.alarcon81@gmail.com>
2023-08-24 18:11:01 +0200

    logica de tamaño menu pantallas grandes

--- bikeandpelotis/src/Components/Header.jsx
+++ bikeandpelotis/src/Components/Header.jsx
@@ -7,18 +7,19 @@ import { Link } from 'react-router-dom'
 function Header() {
     const [isOpen, setIsOpen] = useState(false)
     const handleToggle = () => setIsOpen(!isOpen)
+    const toggleFalse = () => setIsOpen(false)
     const iconMenu = isOpen ? <MdClose className='icon-menu-header'/> : <MdMenu className='icon-menu-header'/>
-    const width = isOpen && screen.width < 992 ? 0 : 100
+    const width = isOpen || screen.width > 992 ? 100 : 0
 
     return (
         <header>
-            <Link to='/' onClick={handleToggle}><img className='logo-ByP-header' src={logo_ByP} /></Link>            
+            <Link to='/' onClick={toggleFalse}><img className='logo-ByP-header' src={logo_ByP} /></Link>            
             <div className='icon-menu-header-container' onClick={handleToggle}>{iconMenu}</div>
             <div className='menu-items-container' style={{width: `${width}vw`}}>
                 <ul className='list-items'>
-                    <li className='menu-item'onClick={handleToggle}><Link to='/'>Inicio</Link></li>
-                    <li className='menu-item'onClick={handleToggle}><Link to='/asedio'>Asedio'23</Link></li>
-                    <li className='menu-item'onClick={handleToggle}><Link to='/4millas'>4Millas'23</Link></li>
+                    <li className='menu-item'onClick={toggleFalse}><Link to='/'>Inicio</Link></li>
+                    <li className='menu-item'onClick={toggleFalse}><Link to='/asedio'>Asedio'23</Link></li>
+                    <li className='menu-item'onClick={toggleFalse}><Link to='/4millas'>4Millas'23</Link></li>
                 </ul> 
             </div>
         </header>

27a13809ee836ba562fba5aceb80669c2bff58b2
AlarconCode <julian.alarcon81@gmail.com>
2023-08-30 20:08:11 +0200

    arreglos menu principal responsive

--- bikeandpelotis/src/Components/Header.jsx
+++ bikeandpelotis/src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo_ByP from '../assets/logo-bikeandpelotis.svg'
 import './header.css'
 import { MdClose, MdMenu } from 'react-icons/md'
@@ -9,17 +9,21 @@ function Header() {
     const handleToggle = () => setIsOpen(!isOpen)
     const toggleFalse = () => setIsOpen(false)
     const iconMenu = isOpen ? <MdClose className='icon-menu-header'/> : <MdMenu className='icon-menu-header'/>
-    const width = isOpen || screen.width > 992 ? 100 : 0
+
+    useEffect(() => {
+        const div = document.getElementById('menu-movil-container')
+        div.style.width = isOpen ? `100vw` : '0'
+    },[isOpen])
 
     return (
         <header>
-            <Link to='/' onClick={toggleFalse}><img className='logo-ByP-header' src={logo_ByP} /></Link>            
+            <Link to='/'><img className='logo-ByP-header' src={logo_ByP}/></Link>            
             <div className='icon-menu-header-container' onClick={handleToggle}>{iconMenu}</div>
-            <div className='menu-items-container' style={{width: `${width}vw`}}>
+            <div className='menu-items-container' id='menu-movil-container'>
                 <ul className='list-items'>
                     <li className='menu-item'onClick={toggleFalse}><Link to='/'>Inicio</Link></li>
-                    <li className='menu-item'onClick={toggleFalse}><Link to='/asedio'>Asedio&apos;23</Link></li>
-                    <li className='menu-item'onClick={toggleFalse}><Link to='/4millas'>4Millas&apos;23</Link></li>
+                    <li className='menu-item'onClick={toggleFalse}><Link to='/asedio'>Asedio'23</Link></li>
+                    <li className='menu-item'onClick={toggleFalse}><Link to='/4millas'>4Millas'23</Link></li>
                 </ul> 
             </div>
         </header>
